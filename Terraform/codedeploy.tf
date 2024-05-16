9961df92ea2f873a20112177a439c54bbec6cb47
unknown <fredrickritchie@gmail.com>
2024-05-16 19:59:44 +0530

    final commit

--- Terraform/codedeploy.tf
+++ Terraform/codedeploy.tf
@@ -8,12 +8,15 @@ resource "aws_codedeploy_deployment_group" "CodeDeployWebapp" {
   deployment_group_name = "CodeDeployWebapp"
   service_role_arn      = aws_iam_role.codedeploy_role.arn
 
-  load_balancer_info {
-    target_group_info  {
-      name = aws_lb_target_group.webapp-TG.name
-        }
-  }
-    autoscaling_groups = [aws_autoscaling_group.webapp_asg.name] # Replace with your Auto Scaling group name
-    deployment_config_name = "CodeDeployDefault.AllAtOnce"  # Use "AllAtOnce" deployment configuration
+  autoscaling_groups  = [aws_autoscaling_group.webapp_asg.name]
 
+  load_balancer_info {
+    target_group_info {
+      name = aws_lb_target_group.webapp-TG.name
+    }
+  }
+    deployment_style {
+    deployment_type = "IN_PLACE"
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+  }
 }
